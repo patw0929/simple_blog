d7416f304b04803a3da14754c58f838c93f6e6e2
patw <patw.hi@gmail.com>
2016-06-08 00:11:16 +0800

    Comment create action

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,20 @@
 class CommentsController < ApplicationController
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.create(comment_params)
+    @comment.post_id = @post.id
+    @comment.author_id = current_author.id
+
+    if @comment.save
+      redirect_to post_path(@post), :notice => "Your comment is posted."
+    else
+      render :action => "new"
+    end
+  end
+
+  private
+
+  def comment_params
+      params.require(:comment).permit(:content)
+  end
 end
