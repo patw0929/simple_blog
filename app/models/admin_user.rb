94eeb62e703b4b69e950f839534912c9e098fb08
patw <patw.hi@gmail.com>
2016-06-07 23:52:07 +0800

    Find admin user data through google oauth

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -1,6 +1,20 @@
 class AdminUser < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, 
-         :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable,
+         :recoverable, :rememberable, :trackable, :validatable, :omniauthable
+
+  def self.find_for_google_oauth2(auth)
+    admin = AdminUser.where(email: auth.info.email).first
+
+    unless admin
+      admin = AdminUser.create(name:     auth.info.name,
+                         provider: auth.provider,
+                         uid:      auth.uid,
+                         email:    auth.info.email,
+                         token:    auth.credentials.token,
+                         password: Devise.friendly_token[0, 20])
+    end
+    admin
+  end
 end
