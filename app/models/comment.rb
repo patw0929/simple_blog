750da137b3ac8ad47c699c7a88396fa8ef58a7ae
patw <patw.hi@gmail.com>
2016-06-08 04:32:44 +0800

    Attach author name to post/comment model as default

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,11 @@ class Comment < ActiveRecord::Base
   belongs_to :post
   belongs_to :author
   scope :recent, -> { order(:created_at => :desc, :id => :desc) }
+
+  def as_json(options={})
+    super(:include => {
+            :author => {:only => [:name]}
+          }
+    )
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,11 @@ class Post < ActiveRecord::Base
   has_many :comments, :dependent => :delete_all
   belongs_to :author
   scope :recent, -> { order(:created_at => :desc, :id => :desc) }
+
+  def as_json(options={})
+    super(:include => {
+            :author => {:only => [:name]}
+          }
+    )
+  end
 end
