5f4c950fb2699bed1de7b1b9dc615191110803ee
patw <patw.hi@gmail.com>
2016-06-07 21:57:49 +0800

    Relation between models

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,5 @@
 class Comment < ActiveRecord::Base
+  belongs_to :post
+  belongs_to :author
+  scope :recent, -> { order(:created_at => :desc, :id => :desc) }
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ActiveRecord::Base
+  has_many :comments, :dependent => :delete_all
+  belongs_to :author
+  scope :recent, -> { order(:created_at => :desc, :id => :desc) }
 end
