0611f842cbad78bd5b466680f9dfa6d379c49826
patw <patw.hi@gmail.com>
2016-06-08 04:29:47 +0800

    Added application#index for server side render

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,10 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  def index
+    @location_path = "/#{params[:path]}"
+  end
+
 
   protected
 
