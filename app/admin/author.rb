f5d3249c2d2f1952cdb3ba0809242e13ca116825
patw <patw.hi@gmail.com>
2016-06-08 01:26:53 +0800

    Revised author's admin resource fields

--- app/admin/author.rb
+++ app/admin/author.rb
@@ -1,18 +1,29 @@
 ActiveAdmin.register Author do
-  permit_params :email, :name
+  permit_params :email, :name, :password, :password_confirmation
 
   index do
+    selectable_column
+    id_column
     column :email
     column :name
     column :current_sign_in_at
-    column :last_sign_in_at
     column :sign_in_count
+    column :created_at
     actions
   end
 
+  filter :email
+  filter :current_sign_in_at
+  filter :sign_in_count
+  filter :created_at
+
   form do |f|
-    f.input :email
-    f.input :name
-    actions
+    f.inputs "Admin Details" do
+      f.input :email
+      f.input :name
+      f.input :password
+      f.input :password_confirmation
+    end
+    f.actions
   end
 end
