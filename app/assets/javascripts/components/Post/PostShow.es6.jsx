99f6d0c4e42175039e7d4137def2b91fcefcd9d8
patw <patw.hi@gmail.com>
2016-06-09 10:34:11 +0800

    Handle delete button

--- app/assets/javascripts/components/Post/PostShow.es6.jsx
+++ app/assets/javascripts/components/Post/PostShow.es6.jsx
@@ -1,5 +1,33 @@
 class PostShow extends React.Component {
+  handleDelete(id) {
+    if (confirm('Do you really want to delete it?')) {
+      fetch(`/api/v1/posts/${id}`, {
+        method: 'DELETE',
+        credentials: 'same-origin',
+        headers: {
+          'X-CSRF-Token': document.querySelector('meta[name="csrf-token"]').getAttribute('content'),
+        },
+      }).then((response) => {
+        if (response.ok) {
+          window.location.href = '/';
+        } else {
+          alert('Failed.');
+        }
+      });
+    }
+  }
+
   render () {
+    AuthorPanel = '';
+    if (this.props.current_author_id == this.props.author_id) {
+      AuthorPanel = (
+        <div className='btn-group'>
+          <Link to={'/posts/' + this.props.id + '/edit'} className='btn btn-default'>Edit</Link>
+          <a className='btn btn-danger' onClick={this.handleDelete.bind(this, this.props.id)}>Delete</a>
+        </div>
+      );
+    }
+
     return (
       <div>
         <h1>{this.props.title}</h1>
@@ -9,6 +37,8 @@ class PostShow extends React.Component {
         </p>
 
         <p>{this.props.content}</p>
+
+        <div>{AuthorPanel}</div>
 
         <hr />
 
@@ -27,4 +57,5 @@ PostShow.defaultProps = {
   author: {
     name: ''
   },
+  author_id: 0,
 };
