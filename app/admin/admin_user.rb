32cdc77818aef63b811642de0a8bbbd2cb930674
patw <patw.hi@gmail.com>
2016-06-16 21:30:53 +0800

    Revised the edit form of admin user

--- app/admin/admin_user.rb
+++ app/admin/admin_user.rb
@@ -1,5 +1,5 @@
 ActiveAdmin.register AdminUser do
-  permit_params :email, :password, :password_confirmation
+  permit_params :email, :password, :password_confirmation, :enable
 
   index do
     selectable_column
@@ -20,9 +20,20 @@ ActiveAdmin.register AdminUser do
   form do |f|
     f.inputs "Admin Details" do
       f.input :email
+      f.input :password
+      f.input :password_confirmation
       f.input :enable
     end
     f.actions
   end
 
+  controller do
+    def update
+      if params[:admin_user][:password].blank? && params[:admin_user][:password_confirmation].blank?
+        params[:admin_user].delete("password")
+        params[:admin_user].delete("password_confirmation")
+      end
+      super
+    end
+  end
 end
